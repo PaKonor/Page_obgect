a9def7829595722c371ac5fecb5529de43505c21
PaKonor <pakonorov89@gmail.com>
2021-10-14 00:55:24 +0300

    добавил неявное ожидание и assert

--- test_main_page.py
+++ test_main_page.py
@@ -6,16 +6,17 @@
 
 from .pages.main_page import MainPage
 
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(fixt):
-    link = "http://selenium1py.pythonanywhere.com/"
+
     page = MainPage(fixt, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера(fixt) и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
 
 def test_guest_should_see_login_link(fixt):
-    link = "http://selenium1py.pythonanywhere.com/"
+
     page = MainPage(fixt, link)
     page.open()
     page.should_be_login_link()
