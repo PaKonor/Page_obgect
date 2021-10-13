1f396589f5de0d75b08df157a60903b43b5d2397
PaKonor <pakonorov89@gmail.com>
2021-10-14 00:25:33 +0300

    добавил неявное ожидание и assert

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,19 @@
 # базовая страница от которой будут унаследованы все остальные классы. В ней мы опишем вспомогательные методы для работы с драйверо
-# В ней мы опишем вспомогательные методы для работы с драйвером
+# В ней мы опишем вспомогательные методы для работы с драйверомfrom selenium.common.exceptions import
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout) # добавили команду для неявного ожидания со значением по умолчанию в 10
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -12,3 +12,10 @@ def test_guest_can_go_to_login_page(fixt):
     page = MainPage(fixt, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера(fixt) и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+
+def test_guest_should_see_login_link(fixt):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(fixt, link)
+    page.open()
+    page.should_be_login_link()
