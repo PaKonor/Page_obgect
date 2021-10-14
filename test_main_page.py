19ff2d320d8d4a02be3174d754ab818643fcc93d
PaKonor <pakonorov89@gmail.com>
2021-10-14 23:03:25 +0300

    la

--- test_main_page.py
+++ test_main_page.py
@@ -11,7 +11,7 @@ link = "http://selenium1py.pythonanywhere.com/"
 def test_guest_can_go_to_login_page(fixt):
 
     page = MainPage(fixt, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера(fixt) и url адрес
-    page.open()                      # открываем страницу
+    page.open()                      # открываем страницу(переходит по сссылке с попощью метода .get)
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
 
